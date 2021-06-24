b51d1777e888b5b95d64c308965d7e5a70d2cd28
bulsond <bulsond@gmail.com>
2021-06-24 23:43:14 +0300

    refact: улучшенный вариант

--- CheckForm/index.js
+++ CheckForm/index.js
@@ -4,18 +4,14 @@ const $email = document.getElementById('email');
 const $password = document.getElementById('password');
 const $password2 = document.getElementById('password2');
 
-function showError($input, message) {
-    const $formControl = $input.parentElement;
-    $formControl.className = 'form-control error';
-    const $small = $formControl.querySelector('small');
-    $small.innerText = message;
-}
-
-function showSuccess($input) {
-    const $formControl = $input.parentElement;
-    $formControl.className = 'form-control success';
-    const $small = $formControl.querySelector('small');
-    $small.innerText = '';
+function checkEmpty({value, parent, small, errorMessages}) {
+    if (value() === '') {
+        parent.className = 'form-control error';
+        small.innerText = errorMessages[0];
+    } else {
+        parent.className = 'form-control success';
+        small.innerText = '';
+    }
 }
 
 function isValidEmail(email) {
@@ -23,34 +19,59 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function checkEmail({value, parent, small, errorMessages}) {
+    if (isValidEmail(value()) === false) {
+        parent.className = 'form-control error';
+        if (small.innerText === '') {
+            small.innerText = errorMessages[1];
+        }
+    } else {
+        parent.className = 'form-control success';
+        small.innerText = '';
+    }
+}
+
+const formInputs = {
+    username: {
+        value: _ => $username.value,
+        parent: $username.parentElement,
+        small: $username.parentElement.querySelector('small'),
+        errorMessages: ['Требуется имя пользователя'],
+        checkRules: [checkEmpty]
+    },
+    email: {
+        value: _ => $email.value,
+        parent: $email.parentElement,
+        small: $email.parentElement.querySelector('small'),
+        errorMessages: ['Требуется email', 'Требуется корректный email'],
+        checkRules: [checkEmpty, checkEmail]
+    },
+    password: {
+        value: _ => $password.value,
+        parent: $password.parentElement,
+        small: $password.parentElement.querySelector('small'),
+        errorMessages: ['Требуется пароль'],
+        checkRules: [checkEmpty]
+    },
+    password2: {
+        value: _ => $password2.value,
+        parent: $password2.parentElement,
+        small: $password2.parentElement.querySelector('small'),
+        errorMessages: ['Требуется повторный пароль'],
+        checkRules: [checkEmpty]
+    },
+};
+
+
+
 // Event listeners
 $form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    if ($username.value === '') {
-        showError($username, 'Требуется имя пользователя');
-    } else {
-        showSuccess($username);
-    }
-
-    if ($email.value === '') {
-        showError($email, 'Требуется email');
-    } else if (isValidEmail($email.value) === false) {
-        showError($email, 'Требуется корректный email');
-    }
-     else {
-        showSuccess($email);
-    }
-
-    if ($password.value === '') {
-        showError($password, 'Требуется пароль');
-    } else {
-        showSuccess($password);
-    }
-
-    if ($password2.value === '') {
-        showError($password2, 'Требуется повторный пароль');
-    } else {
-        showSuccess($password2);
+    for (const key in formInputs) {
+        const element = formInputs[key];
+        for (const rule of element.checkRules) {
+            rule({...element});
+        }
     }
 });
